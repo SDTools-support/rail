726792cee04d6c3bb964da8fc660d2c753fa4bd2
Etienne Balmes <61470600+etiennebalmes@users.noreply.github.com>
2024-04-23 18:21:15 +0200

    eb

--- sdt_config.m
+++ sdt_config.m
@@ -1,7 +1,8 @@
 
 if sd('_user','balmes') % BALMES
  pw1=pwd;
- sd _pdynavoie
+ if ~exist('dyn_utils','file');sd _pdynavoie;end
+ if ~exist('rail19','file');sd _psncf_ir;end
  addpath(fullfile(pw1,'m'));clear pw1;
  pw0=comgui('cd /o/sdtdata/rail19');
  if ~exist(pw0,'dir');pw0='d:/balmes';end
